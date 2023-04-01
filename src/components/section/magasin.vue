0a9686fce358eb119b5019c47f4d7d84b5fd3c7d
kionou <kionoumamadou.00@gmail.com>
2023-04-01 17:54:30 +0000

    ok

--- src/components/section/magasin.vue
+++ src/components/section/magasin.vue
@@ -3,7 +3,6 @@
         <div class="container">
             <div class="maps">
                 <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1H-947mLzNVKwthcdPGj6DVA5q2fqMfQ&ehbc=2E312F" width="640" height="480"></iframe>
-
             </div>
             
         </div>
